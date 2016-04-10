bf5ecabf0ecdc794c9c5c94cc3688eb2a3e1e366
Riadhi Aditya <ridzeal@gmail.com>
2016-04-11 00:20:22 +0700

    Switch from materialize-css to material2

--- app/components/app.ts
+++ app/components/app.ts
@@ -2,6 +2,17 @@ import {Component} from 'angular2/core';
 import {RouteConfig,
         ROUTER_DIRECTIVES,
         ROUTER_PROVIDERS} from 'angular2/router';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
+import {MdSpinner} from '@angular2-material/progress-circle';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+
 import {ProductsComponent} from './products';
 import {DashboardComponent} from './dashboard';
 import {ProductDetailComponent} from './product-detail';
@@ -15,6 +26,15 @@ import {LoggerService} from './../services/logger'
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
+    MD_SIDENAV_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdToolbar,
+    MdButton,
+    MdCheckbox,
+    MdRadioButton,
+    MdSpinner,
+    MD_INPUT_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
     ProductService,
     LoggerService
   ]
